085452877ccfcddbd5558de1b8d2faf006c1f8e5
Andrey Gubarev <mylokin@me.com>
2015-02-18 02:34:58 +0300

    update tests

--- tests/test_mongoext.py
+++ tests/test_mongoext.py
@@ -5,7 +5,7 @@ import mongoext.collection
 import mongoext.models
 
 
-class MyCollection(mongoext.collection.Collection):
+class Collection(mongoext.collection.Collection):
     CONNECTION = {'host': 'localhost', 'port': 27017}
     DATABASE = 'db1'
     NAME = 'collection1'
@@ -15,18 +15,29 @@ class MyCollection(mongoext.collection.Collection):
     }
 
 
-class MyModel(mongoext.models.Model):
-    objects = MyCollection()
+class Model(mongoext.models.Model):
+    objects = Collection()
 
     about = mongoext.fields.Numeric()
 
 
 class Mongoext(unittest.TestCase):
-    def test_mongoext(self):
-        cursor = MyCollection().find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
-        print [c for c in cursor]
-        cursor = MyModel.objects.find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
-        m = [c for c in cursor][0]
-        print m
-        m.save()
-        print m
+    def test_collection_find(self):
+        cursor = Collection().find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
+        self.assertTrue(all([lambda: isinstance(c, dict) for c in cursor]))
+
+    def test_model_find(self):
+        cursor = Model.objects.find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
+        self.assertTrue(all([lambda: isinstance(c, Model) for c in cursor]))
+
+    def test_model_save(self):
+        cursor = Model.objects.find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
+        model = [c for c in cursor][0]
+        model.save()
+
+    def test_model_fail_save(self):
+        cursor = Model.objects.find({'about': {'$gte': 1}}, {'about': 1}, 1).sort('about')
+        model = [c for c in cursor][0]
+        model.about = None
+        with self.assertRaises(TypeError):
+            model.save()
