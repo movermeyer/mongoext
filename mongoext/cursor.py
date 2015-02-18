04b5331d93f0ed1b907b76ccc9dc4c1685316ebd
Andrey Gubarev <mylokin@me.com>
2015-02-18 03:01:14 +0300

    make pymongo_cursor private

--- mongoext/cursor.py
+++ mongoext/cursor.py
@@ -4,14 +4,14 @@ from __future__ import absolute_import
 class Cursor(object):
     def __init__(self, collection, pymongo_cursor):
         self.collection = collection
-        self.pymongo_cursor = pymongo_cursor
+        self.__pymongo_cursor = pymongo_cursor
 
     def sort(self, *args, **kw):
-        self.pymongo_cursor = self.pymongo_cursor.sort(*args, **kw)
+        self.__pymongo_cursor = self.__pymongo_cursor.sort(*args, **kw)
         return self
 
     def __iter__(self):
-        for document in self.pymongo_cursor:
+        for document in self.__pymongo_cursor:
             document = self.collection.unpack_fields(document)
             if self.collection.model:
                 yield self.collection.model(**document)
