3aed83d58f248c50282ed926038dd115edc0ad44
Andrey Gubarev <mylokin@me.com>
2015-04-21 17:38:57 +0300

    fix classifiers

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
         'Topic :: Database',
         'Topic :: Database :: Database Engines/Servers',
     ),
