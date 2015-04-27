ed4d049e74a9e9034497b78a8bb4f6e90acf47e8
Andrey Gubarev <mylokin@me.com>
2015-04-28 00:36:13 +0300

    comment out fields tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,146 +1,146 @@
-import datetime
-import unittest
+# import datetime
+# import unittest
 
-import mongoext.scheme as scheme
-import mongoext.exc as exc
-
-
-class TestNumeric(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.Numeric()
-
-    def test_str_number(self):
-        self.assertEqual(self.field.cast('1'), 1)
-
-    def test_unsupported_str(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast('a')
+# import mongoext.scheme as scheme
+# import mongoext.exc as exc
 
 
-class TestUnicodeField(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.Unicode()
+# class TestNumeric(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.Numeric()
 
-    def test_int(self):
-        self.assertEqual(self.field.cast(1), u'1')
+#     def test_str_number(self):
+#         self.assertEqual(self.field.cast('1'), 1)
 
-    def test_zero(self):
-        self.assertEqual(self.field.cast(0), u'0')
-
-    def test_true(self):
-        self.assertEqual(self.field.cast(True), u'True')
+#     def test_unsupported_str(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast('a')
 
 
-class TestList(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.List()
+# class TestUnicodeField(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.Unicode()
 
-    def test_random_values(self):
-        self.assertEqual(self.field.cast([1, '1']), [1, '1'])
+#     def test_int(self):
+#         self.assertEqual(self.field.cast(1), u'1')
 
-    def test_empty_list(self):
-        self.assertEqual(self.field.cast([]), [])
+#     def test_zero(self):
+#         self.assertEqual(self.field.cast(0), u'0')
 
-    def test_non_iterable_none(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(None)
-
-    def test_non_iterable_int(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(1)
+#     def test_true(self):
+#         self.assertEqual(self.field.cast(True), u'True')
 
 
-class TestNumericListField(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.List(scheme.Numeric())
+# class TestList(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.List()
 
-    def test_list_of_valid_strings(self):
-        self.assertEqual(self.field.cast(['1', '2']), [1, 2])
+#     def test_random_values(self):
+#         self.assertEqual(self.field.cast([1, '1']), [1, '1'])
 
-    def test_empty_list(self):
-        self.assertEqual(self.field.cast([]), [])
+#     def test_empty_list(self):
+#         self.assertEqual(self.field.cast([]), [])
 
-    def test_list_of_invalid_strings(self):
-        with self.assertRaises(exc.CastError):
-            self.assertEqual(self.field.cast(['a', '2']), ['a', 2])
+#     def test_non_iterable_none(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(None)
 
-    def test_non_iterable_none(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(None)
-
-    def test_non_iterable_int(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(1)
+#     def test_non_iterable_int(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(1)
 
 
-class TestInvalidListField(unittest.TestCase):
-    def test_int(self):
-        with self.assertRaises(exc.SchemeError):
-            scheme.List(int)
+# class TestNumericListField(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.List(scheme.Numeric())
 
-    def test_class_field(self):
-        with self.assertRaises(exc.SchemeError):
-            scheme.List(scheme.Numeric)
+#     def test_list_of_valid_strings(self):
+#         self.assertEqual(self.field.cast(['1', '2']), [1, 2])
 
+#     def test_empty_list(self):
+#         self.assertEqual(self.field.cast([]), [])
 
-class TestDateTimeAutoadd(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.DateTime(autoadd=True)
+#     def test_list_of_invalid_strings(self):
+#         with self.assertRaises(exc.CastError):
+#             self.assertEqual(self.field.cast(['a', '2']), ['a', 2])
 
-    def test_auto_add(self):
-        dt = self.field.cast(None)
-        self.assertTrue(isinstance(dt, datetime.datetime))
+#     def test_non_iterable_none(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(None)
 
-
-class TestDateTime(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.DateTime()
-
-    def test_auto_add(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(None)
+#     def test_non_iterable_int(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(1)
 
 
-class TestDict(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.Dict()
+# class TestInvalidListField(unittest.TestCase):
+#     def test_int(self):
+#         with self.assertRaises(exc.SchemeError):
+#             scheme.List(int)
 
-    def test_random_values(self):
-        self.assertEqual(self.field.cast({'a': 1}), {'a': 1})
-
-    def test_empty_dict(self):
-        self.assertEqual(self.field.cast({}), {})
-
-    def test_non_mapping_none(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(None)
-
-    def test_non_mapping_int(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(1)
+#     def test_class_field(self):
+#         with self.assertRaises(exc.SchemeError):
+#             scheme.List(scheme.Numeric)
 
 
-class TestNumericDict(unittest.TestCase):
-    def setUp(self):
-        self.field = scheme.Dict(scheme.Numeric())
+# class TestDateTimeAutoadd(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.DateTime(autoadd=True)
 
-    def test_random_values(self):
-        self.assertEqual(self.field.cast({'a': 1}), {'a': 1})
+#     def test_auto_add(self):
+#         dt = self.field.cast(None)
+#         self.assertTrue(isinstance(dt, datetime.datetime))
 
-    def test_random_valid_values(self):
-        self.assertEqual(self.field.cast({'a': '1'}), {'a': 1})
 
-    def test_empty_dict(self):
-        self.assertEqual(self.field.cast({}), {})
+# class TestDateTime(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.DateTime()
 
-    def test_invalid_values(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast({'a': 'a'})
+#     def test_auto_add(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(None)
 
-    def test_non_mapping_none(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(None)
 
-    def test_non_mapping_int(self):
-        with self.assertRaises(exc.CastError):
-            self.field.cast(1)
+# class TestDict(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.Dict()
+
+#     def test_random_values(self):
+#         self.assertEqual(self.field.cast({'a': 1}), {'a': 1})
+
+#     def test_empty_dict(self):
+#         self.assertEqual(self.field.cast({}), {})
+
+#     def test_non_mapping_none(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(None)
+
+#     def test_non_mapping_int(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(1)
+
+
+# class TestNumericDict(unittest.TestCase):
+#     def setUp(self):
+#         self.field = scheme.Dict(scheme.Numeric())
+
+#     def test_random_values(self):
+#         self.assertEqual(self.field.cast({'a': 1}), {'a': 1})
+
+#     def test_random_valid_values(self):
+#         self.assertEqual(self.field.cast({'a': '1'}), {'a': 1})
+
+#     def test_empty_dict(self):
+#         self.assertEqual(self.field.cast({}), {})
+
+#     def test_invalid_values(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast({'a': 'a'})
+
+#     def test_non_mapping_none(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(None)
+
+#     def test_non_mapping_int(self):
+#         with self.assertRaises(exc.CastError):
+#             self.field.cast(1)
