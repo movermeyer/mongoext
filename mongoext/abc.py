b059f1198aa4eb0675e1d08bc6421ff7d4f3d2c0
Andrey Gubarev <mylokin@me.com>
2015-06-12 16:23:56 +0300

    update abstract field

--- mongoext/abc.py
+++ mongoext/abc.py
@@ -2,18 +2,15 @@ import weakref
 
 
 class AbstractField(object):
-    def __init__(self, **kw):
+    def __init__(self, descriptors):
+        self.descriptors = descriptors
         self.data = weakref.WeakKeyDictionary()
 
     def __get__(self, instance, owner):
         return self.data.get(instance)
 
     def __set__(self, instance, value):
-        value = self.process(value)
         self.data[instance] = value
 
     def __delete__(self, instance):
         del self.data[instance]
-
-    def process(self, value):
-        raise NotImplementedError
