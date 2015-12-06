ee535cc03efe2496626aab198405edf9d6c20567
Andrey Gubarev <mylokin@me.com>
2015-12-06 03:08:56 +0300

    process fields using abstract fields data descriptors

--- mongoext/abc.py
+++ mongoext/abc.py
@@ -2,8 +2,8 @@ import weakref
 
 
 class AbstractField(object):
-    def __init__(self, fields):
-        self.fields = fields
+    def __init__(self, field):
+        self.field = field
         self.data = weakref.WeakKeyDictionary()
 
     def __get__(self, instance, owner):
@@ -14,7 +14,10 @@ class AbstractField(object):
             del self.data[instance]
             return
 
-        self.data[instance] = self.fields(value)
+        self.data[instance] = self.field(value)
 
     def __delete__(self, instance):
         del self.data[instance]
+
+    def __call__(self, value):
+        return self.field(value)
