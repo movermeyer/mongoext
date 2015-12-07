a67247b386e90e734cac6fc2101a1ad8949d86b5
Andrey Gubarev <mylokin@me.com>
2015-12-07 07:06:43 +0300

    drop mongoext abc

--- mongoext/document.py
+++ mongoext/document.py
@@ -1,13 +1,36 @@
 from __future__ import absolute_import
 
-import mongoext.abc
+import weakref
+
 import mongoext.collection
 import mongoext.scheme
 import mongoext.exc
 
 
+class AbstractField(object):
+    def __init__(self, field):
+        self.field = field
+        self.data = weakref.WeakKeyDictionary()
+
+    def __get__(self, instance, owner):
+        return self.data.get(instance)
+
+    def __set__(self, instance, value):
+        if value is None and instance in self.data:
+            del self.data[instance]
+            return
+
+        self.data[instance] = self.field(value)
+
+    def __delete__(self, instance):
+        del self.data[instance]
+
+    def __call__(self, value):
+        return self.field(value)
+
+
 class MetaSchema(type):
-    FIELD = (mongoext.abc.AbstractField, mongoext.scheme.Field)
+    FIELD = (AbstractField, mongoext.scheme.Field)
 
     def __new__(cls, class_name, bases, attrs):
         scheme = {}
@@ -20,8 +43,8 @@ class MetaSchema(type):
                 scheme[name] = obj
         attrs['_scheme'] = scheme
         for name, field in scheme.items():
-            if not isinstance(field, mongoext.abc.AbstractField):
-                field = mongoext.abc.AbstractField(field)
+            if not isinstance(field, AbstractField):
+                field = AbstractField(field)
             attrs[name] = field
         return super(MetaSchema, cls).__new__(cls, class_name, bases, attrs)
 
